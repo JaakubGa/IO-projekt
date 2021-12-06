ad8901679ab7c4fca78455f32b616151b57a6020
Dominik Sanak <dominik.sanak159@gmail.com>
2021-12-06 19:25:20 +0100

    Testy2 - odwołoywanie się do gałęzi

---
+++ wypozyczalnia_produkcja/Sabina/trest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wypozyczalnia_produkcja.Sabina
+{
+  public static class trest
+    {
+        public static int MyProperty = 2;
+    }
+}
