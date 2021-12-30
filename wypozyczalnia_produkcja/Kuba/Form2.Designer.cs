b38772f424a7cf53db1173566572975a75c1f503
Jakub Gasidło <nexunek@gmail.com>
2021-12-30 22:47:00 +0100

    Ulepszanie interfejsu

--- wypozyczalnia_produkcja/Kuba/Form2.Designer.cs
+++ wypozyczalnia_produkcja/Kuba/Form2.Designer.cs
@@ -39,6 +39,7 @@ namespace wypozyczalnia_produkcja
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -88,7 +89,7 @@ namespace wypozyczalnia_produkcja
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(35, 189);
+            this.button1.Location = new System.Drawing.Point(35, 217);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(129, 39);
             this.button1.TabIndex = 2;
@@ -133,11 +134,21 @@ namespace wypozyczalnia_produkcja
             this.label3.TabIndex = 3;
             this.label3.Text = "Cena";
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(242, 573);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 39);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Wróć do narzędzi";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(935, 624);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.panel1);
@@ -164,5 +175,6 @@ namespace wypozyczalnia_produkcja
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
     }
 }
