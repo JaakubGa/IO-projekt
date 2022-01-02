9af3450475ccf6157d8def3773ea81d7c8295ffa
Dominik Sanak <dominik.sanak159@gmail.com>
2022-01-02 22:04:42 +0100

    Dodanie testowej bazy danych

---
+++ wypozyczalnia/DSanak/bazadanych_test/dbo.logowanie.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [login] NVARCHAR(50) NULL, 
+    [password] NVARCHAR(50) NULL, 
+    [e_mail] NVARCHAR(50) NULL, 
+    [phone_number] NCHAR(10) NULL
+)
