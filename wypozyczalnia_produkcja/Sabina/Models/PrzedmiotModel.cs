4eb584ed3d151f1bd44ab95976c3111a1275432e
Sabina <iwaneksabina8@gmail.com>
2022-01-22 20:08:56 +0100

    Poprawa modeli i połączenia

--- wypozyczalnia_produkcja/Sabina/Models/PrzedmiotModel.cs
+++ wypozyczalnia_produkcja/Sabina/Models/PrzedmiotModel.cs
@@ -24,12 +24,10 @@ namespace wypozyczalnia_produkcja.Sabina.Models
         //CTOR
         public PrzedmiotModel(int id)
         {
-
-            using (Connect.Connection)
+            using (SqlConnection Connection = new SqlConnection(Connect.StringConnection))
             {
-                SqlCommand command = new SqlCommand($"SELECT * FROM Sprzet WHERE id_sprzetu = {id}", Connect.Connection);
-                Connect.Connection.Open();
-
+                SqlCommand command = new SqlCommand($"SELECT * FROM Sprzet WHERE id_sprzetu = {id}", Connection);
+                Connection.Open();
                 SqlDataReader reader = command.ExecuteReader();
 
                 //wpisanie danych z bazy
@@ -42,33 +40,35 @@ namespace wypozyczalnia_produkcja.Sabina.Models
                     Datadostepnosci = reader[4].ToString();
                     Cena = (decimal)reader[5];
                 }
+
                 reader.Close();
-                Connect.Connection.Close();
+                Connection.Close();
             }
-            DopiszDaneZId();
+            DopiszKategorie();
+            DopiszUzytkownika();
         }
 
-        public void DopiszDaneZId()
+        public void DopiszKategorie()
         {
-            /*using (SqlConnection connection = new SqlConnection("Data Source=LAPTOP-Q6JBHJK9;Initial Catalog=Wypozyczalnia;Integrated Security=True"))
+            using (SqlConnection Connection = new SqlConnection(Connect.StringConnection))
             {
-                SqlCommand command = new SqlCommand($"SELECT nazwa FROM Kategoria WHERE id_kategorii = 6", connection);
-                connection.Open();
-
+                SqlCommand command = new SqlCommand($"SELECT nazwa FROM Kategoria WHERE id_kategorii = {Idkategoria}", Connection);
+                Connection.Open();
                 SqlDataReader reader = command.ExecuteReader();
 
                 //wpisanie danych z bazy
                 while (reader.Read())
                 {
-                    ID = (int)reader[0];
-                    IdWlasciciela = (int)reader[1];
-                    Idkategoria = (int)reader[2];
-                    Nazwa = reader[3].ToString();
-                    Datadostepnosci = reader[4].ToString();
-                    Cena = (decimal)reader[5];
+                    Kategoria = (string)reader[0];
                 }
+
                 reader.Close();
-            }*/
-        } 
+                Connection.Close();
+            }
+        }
+        public void DopiszUzytkownika()
+        {
+            uzytkownik = new Uzytkownik(IdWlasciciela);
+        }
     }
 }
--- wypozyczalnia_produkcja/Sabina/Models/Uzytkownik.cs
+++ wypozyczalnia_produkcja/Sabina/Models/Uzytkownik.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using wypozyczalnia_produkcja.Sabina.Connection;
 
 namespace wypozyczalnia_produkcja.Sabina.Models
 {
     public class Uzytkownik
     {
+        public int Id;
+        public int IdAdresu;
+        public string Imie;
+        public string Nazwisko;
+        public string Nick;
+        public string Telefon = string.Empty;
+        public string Email = string.Empty;
+        public Adres Adres;
+
+        public Uzytkownik(int id)
+        {
+            using (SqlConnection Connection = new SqlConnection(Connect.StringConnection))
+            {
+
+                SqlCommand command = new SqlCommand($"SELECT * FROM Uzytkownik WHERE id_Uzytkownika = {id}", Connection);
+                Connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                //wpisanie danych z bazy
+                while (reader.Read())
+                {
+                    Id = (int)reader[0];
+                    IdAdresu = (int)reader[1];
+                    Imie = reader[2].ToString();
+                    Nazwisko = reader[3].ToString();
+                    Nick = reader[4].ToString();
+                    Telefon = (string)reader[5];
+                    Email = reader[6].ToString();
+                }
+
+                reader.Close();
+                Connection.Close();
+            }
+        }
+        public void DopiszAdres()
+        {
+            Adres = new Adres(IdAdresu);
+        }
     }
 }
