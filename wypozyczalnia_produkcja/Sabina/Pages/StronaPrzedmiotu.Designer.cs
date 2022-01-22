39c9e381e4b5e0d16b6f09f0206be555d38debe8
Sabina <iwaneksabina8@gmail.com>
2022-01-22 12:54:26 +0100

    Konfiguracja strony przedmiotu

--- wypozyczalnia_produkcja/Sabina/Pages/StronaPrzedmiotu.Designer.cs
+++ wypozyczalnia_produkcja/Sabina/Pages/StronaPrzedmiotu.Designer.cs
@@ -1,8 +1,13 @@
 ﻿
+using System.Windows.Forms;
+
 namespace wypozyczalnia_produkcja.Sabina.Pages
 {
     partial class StronaPrzedmiotu
     {
+        //Public
+        
+
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -30,25 +35,47 @@ namespace wypozyczalnia_produkcja.Sabina.Pages
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.labelNazwaPrzedmiotu = new System.Windows.Forms.Label();
+            this.labelNickWlasciciela = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(50, 305);
+            this.button1.Location = new System.Drawing.Point(67, 375);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(146, 24);
+            this.button1.Size = new System.Drawing.Size(195, 30);
             this.button1.TabIndex = 0;
             this.button1.Text = "Wróć do wyszukiwarki";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // labelNazwaPrzedmiotu
+            // 
+            this.labelNazwaPrzedmiotu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelNazwaPrzedmiotu.Location = new System.Drawing.Point(67, 45);
+            this.labelNazwaPrzedmiotu.Name = "labelNazwaPrzedmiotu";
+            this.labelNazwaPrzedmiotu.Size = new System.Drawing.Size(614, 27);
+            this.labelNazwaPrzedmiotu.TabIndex = 1;
+            // 
+            // labelNickWlasciciela
+            // 
+            this.labelNickWlasciciela.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelNickWlasciciela.Location = new System.Drawing.Point(629, 199);
+            this.labelNickWlasciciela.Name = "labelNickWlasciciela";
+            this.labelNickWlasciciela.Size = new System.Drawing.Size(100, 23);
+            this.labelNickWlasciciela.TabIndex = 2;
+            this.labelNickWlasciciela.Text = "nick";
+            // 
             // StronaPrzedmiotu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelNickWlasciciela);
+            this.Controls.Add(this.labelNazwaPrzedmiotu);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "StronaPrzedmiotu";
             this.Text = "{Nazwa przedmiotu z bazy}";
             this.Load += new System.EventHandler(this.StronaPrzedmiotu_Load);
@@ -59,5 +86,7 @@ namespace wypozyczalnia_produkcja.Sabina.Pages
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private Label labelNazwaPrzedmiotu;
+        private Label labelNickWlasciciela;
     }
 }
