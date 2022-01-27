0307634c558204d4d2eb41994ed3654b72b9d916
Sabina <iwaneksabina8@gmail.com>
2022-01-28 00:16:34 +0100

    Dodanie modelu zamówienie

---
+++ wypozyczalnia_produkcja/Models/Sprzet.cs
@@ -0,0 +1,71 @@
+﻿using System.Data.SqlClient;
+using wypozyczalnia_produkcja.Params;
+
+//sabina
+namespace wypozyczalnia_produkcja.Models
+{
+    public class PrzedmiotModel
+    {
+        //public
+        public int Id;
+        public int IdWlasciciela;
+        public int Idkategoria;
+        public string Nazwa;
+        public string Datadostepnosci;
+        public decimal Cena;
+        public string Kategoria;
+        public Uzytkownik Uzytkownik;
+
+        //CTOR
+        public PrzedmiotModel(int id)
+        {
+            using (SqlConnection Connection = new SqlConnection(Connect.StringConnection))
+            {
+                SqlCommand command = new SqlCommand($"SELECT * FROM Sprzet WHERE id_sprzetu = {id}", Connection);
+                Connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                //wpisanie danych z bazy
+                while (reader.Read())
+                {
+                    Id = (int)reader[0];
+                    IdWlasciciela = (int)reader[1];
+                    Idkategoria = (int)reader[2];
+                    Nazwa = reader[3].ToString();
+                    Datadostepnosci = reader[4].ToString();
+                    Cena = (decimal)reader[5];
+                }
+
+                reader.Close();
+                Connection.Close();
+            }
+            DopiszKategorie();
+            DopiszUzytkownika();
+        }
+
+        public void DopiszKategorie()
+        {
+            using (SqlConnection Connection = new SqlConnection(Connect.StringConnection))
+            {
+                SqlCommand command = new SqlCommand($"SELECT nazwa FROM Kategoria WHERE id_kategorii = {Idkategoria}", Connection);
+                Connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                //wpisanie danych z bazy
+                while (reader.Read())
+                {
+                    Kategoria = (string)reader[0];
+                }
+
+                reader.Close();
+                Connection.Close();
+            }
+        }
+
+        //methods
+        public void DopiszUzytkownika()
+        {
+            Uzytkownik = new Uzytkownik(IdWlasciciela);
+        }
+    }
+}
---
+++ wypozyczalnia_produkcja/Models/Zamowienie.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.SqlClient;
+using wypozyczalnia_produkcja.Params;
+
+//sabina
+namespace wypozyczalnia_produkcja.Models
+{
+    public class Zamowienie
+    {
+        public int Id;
+        public int IdUzytkownika;
+        public int IdSprzetu;
+        public string DataOd;
+        public string DataDo;
+        public decimal Cena;
+        public Uzytkownik uzytkownik;
+        public Sprzet sprzet;
+
+        public Zamowienie(int id)
+        {
+            using (SqlConnection Connection = new SqlConnection(Connect.StringConnection))
+            {
+                SqlCommand command = new SqlCommand($"SELECT * FROM Adres WHERE id_adresu = {id}", Connection);
+                Connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                //wpisanie danych z bazy
+                while (reader.Read())
+                {
+                    Id = (int)reader[0];
+                    IdUzytkownika = (int)reader[1];
+                    IdSprzetu = (int)reader[2];
+                    DataOd = reader[3].ToString();
+                    DataDo = reader[4].ToString();
+                    Cena = (decimal)reader[5];
+                }
+
+                reader.Close();
+                Connection.Close();
+            }
+        }
+    }
+}
