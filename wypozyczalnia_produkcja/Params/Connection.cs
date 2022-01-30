56d3fba11245c771118af8771dbd29239a0c8574
Dominik Sanak <dominik.sanak159@gmail.com>
2022-01-30 21:15:40 +0100

    Dodanie globalnego connection Stringa. Podpięcie przycisków Logowanie oraz Rejestracja

--- wypozyczalnia_produkcja/Params/Connection.cs
+++ wypozyczalnia_produkcja/Params/Connection.cs
@@ -6,9 +6,9 @@ namespace wypozyczalnia_produkcja.Params
         //sabina
         //public static string StringConnection = "Data Source=LAPTOP-Q6JBHJK9;Initial Catalog=Wypozyczalnia;Integrated Security=True";
         //hubert
-        public static string StringConnection = @"Data Source=DESKTOP-J9VF8D6\HUBERT;Initial Catalog=Wypozyczalnia;Integrated Security=True";
+       // public static string StringConnection = @"Data Source=DESKTOP-J9VF8D6\HUBERT;Initial Catalog=Wypozyczalnia;Integrated Security=True";
         //sanak
-        //public static string StringConnection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Wypozyczalnia;Integrated Security=True";
+        public static string StringConnection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Wypozyczalnia;Integrated Security=True";
         //kuba
         //public static string StringConnection = "Data Source=LAPTOP-Q6JBHJK9;Initial Catalog=Wypozyczalnia;Integrated Security=True";
     }
