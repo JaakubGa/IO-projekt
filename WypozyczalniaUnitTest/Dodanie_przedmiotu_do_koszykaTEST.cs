5fd8f0bc0417bc38d46200ee77cd9f48ed218fff
Dominik Sanak <dominik.sanak159@gmail.com>
2022-02-04 19:07:37 +0100

    Testy jednostkowe - test dodawania przedmiotu do koszyka

---
+++ WypozyczalniaUnitTest/Dodanie_przedmiotu_do_koszykaTEST.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wypozyczalnia_produkcja.Pages;
+
+namespace WypozyczalniaUnitTest
+{
+    class Dodanie_przedmiotu_do_koszykaTEST
+    {
+        public void Dodawnie()
+        {
+            //last item == ten dodawany
+            
+                
+
+        }
+
+
+    }
+}
