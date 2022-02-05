81bf99eda1ae78c3eb08a04f0580e70bfc8f7197
Hubert Śpila <hubertspila@gmail.com>
2022-02-05 20:36:34 +0100

    Test jednostkowy Hubert

---
+++ WypozyczalniaUnitTest/Test_Hubert.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wypozyczalnia_produkcja.Models;
+
+namespace WypozyczalniaUnitTest
+{
+    class Test_Hubert
+    {
+        [Test]
+        public void PobieranieUzytkownika()
+        {
+            //arrange
+            Uzytkownik user;
+            Adres adres;
+            Sprzet sprzet;
+
+            //act
+            user = new Uzytkownik(2);
+            var result = user.Id;
+
+            adres = user.Adres;
+            var result2 = adres.Id;
+
+            sprzet = new Sprzet(6);
+            var result3 = sprzet.Uzytkownik.Id;
+
+            //assert
+            Assert.AreEqual(2, result);
+            Assert.AreEqual(user.IdAdresu, result2);
+            Assert.AreEqual(sprzet.IdWlasciciela, result3);
+        }
+    }
+}
+
