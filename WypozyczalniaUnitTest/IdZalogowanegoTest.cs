6ca34887ec815ff9c0804afe7d451efe3ff95d67
SabinaIwanek <sabina.iwanek@bst.com.pl>
2022-02-05 20:40:12 +0100

    Test jednostkowy Sabina

---
+++ WypozyczalniaUnitTest/IdZalogowanegoTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace WypozyczalniaUnitTest
+{
+    //sabina
+    public class IdZalogowanegoTest
+    {
+        int _idZalogowanego;
+        bool _metoda;
+        bool metoda;
+        public int IdZalogowanego
+        {
+            get { return _idZalogowanego; }
+            set
+            {
+                _idZalogowanego = value;
+                _metoda = true;
+                var metoda = true;
+            }
+        }
+
+        [Test]
+        public void PobieranieUzytkownika()
+        {
+            //arrange
+            IdZalogowanego = 1;
+            //act
+            var result = _idZalogowanego;
+            //assert
+            Assert.AreEqual(1, result);
+            Assert.IsTrue(_metoda);
+            Assert.IsTrue(metoda);
+        }
+    }
+}
